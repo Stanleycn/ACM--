161707926981d47590b44f61088764982aab6656
stonetigerking <2445382460@qq.com>
2021-03-15 13:41:38 +0800

    2021/3/15

--- c++acm-problems/4-11guizishou-game.c
+++ c++acm-problems/4-11guizishou-game.c
@@ -20,18 +20,24 @@ int main()
     int rnd;
     while(scanf("%d%s%s",&rnd,s,s2)==3 && rnd !=-1 )
     {
-        printf("Round %d\n",rnd);
-        win=lose=0;
-        leftt=strlen(s);
+        printf("Round %d\n",rnd);//第几回合
+        win=lose=0;   //求解一组新数据前要初始化
+        leftt=strlen(s);//单词长度
         chance =7;
         for(int i=0;i<strlen(s2);i++)
         {
-            guess(s2[i]);
-            if(win||lose) break;
+            guess(s2[i]);    //猜一个字母
+            if(win||lose) break; //检查状态
         }
+        //根据结果输出
         if(win) printf("You win.\n");
         else if(lose) printf("You lose.\n");
         else printf("You chickened out.\n");
     }
     return 0;
 }
+
+
+//题目描述给一个单词，你最多只能错6次，每次猜中后会显示所有猜中的字母
+
+//解答思路：用chance标记次数，每次遍历单词，若相同就置零，且单词长度减一，知道为0.
